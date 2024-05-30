a077d10c483b4ffd28e8a7406ca53b494acce958
YawOtuo <yotuo2003@gmail.com>
2024-05-30 23:27:18 +0000

    "Refactor globals.css to simplify styles, add Tailwind classes for headings, and update layout.tsx and page.tsx to include a Navbar component and a HeroSection component, respectively, removing existing complex styling and layout."

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,54 @@
+import Button from "@/components/Button";
+import styles from "./index.module.css";
+
+function HeroSection() {
+  type SlideProps = {
+    heading: string;
+    subheading: string;
+    button?: string;
+    image: string;
+  };
+  const Slide = ({ heading, subheading, button, image }: SlideProps) => (
+    <div
+      className={`w-full h-full flex flex-col items-start justify-center px-20 gap-5 ${styles.heroSlide} bg-cover bg-no-repeat`}
+      style={{ backgroundImage: `url(${image})` }}>
+      <div>
+        <h3 className="font-semibold">{heading}</h3>
+        <h1 className="uppercase font-bold">{subheading}</h1>
+      </div>
+
+      <Button
+        content={"Shop Now"}
+        className="uppercase !px-14 !py-3 font-semibold"
+        variant="solid"
+        color="red-600"
+        textColor="white"
+        rounded="2xl"
+      />
+    </div>
+  );
+
+  return (
+    <div
+      className={`w-full flex flex-col items-start justify-center gap-5  h-[70vh] ${styles.heroSlideshowContainer}`}>
+      <Slide
+        heading="Quality Afforfable Building One Place"
+        subheading=" All the leading brands in one place"
+        image="/hero/slide_3.jpg"
+      />
+
+      <Slide
+        heading="Quality Afforfable Building One Place"
+        subheading=" All the leading brands in one place"
+        image="/hero/slide_1.jpg"
+      />
+      <Slide
+        heading="Quality Afforfable Building One Place"
+        subheading=" All the leading brands in one place"
+        image="/hero/slide_3.jpg"
+      />
+    </div>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const links = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Store",
+    url: "/store",
+  },
+  {
+    name: "Warehousing",
+    url: "/warehousing",
+  },
+  {
+    name: "Contact",
+    url: "/contact",
+  },
+];
+function Navbar() {
+  return (
+    <div className="flex items-center justify-center w-full py-5 px-5">
+      <div className="relative w-full aspect-[178/36] max-w-[180px]">
+        {" "}
+        <Image src={"/logo-shop.png"} alt="Logo" fill objectFit="cover" />
+      </div>
+
+      <div className="flex items-center gap-5 w-full justify-center">
+        {
+            links?.map((r, index) => (
+              <Link href={r?.url} key={index}>{r.name}</Link>
+            ))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
