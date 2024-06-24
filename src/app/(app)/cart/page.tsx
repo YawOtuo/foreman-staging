5562ad1ddb0456ea05159c7385116ae14a87af85
YawOtuo <yotuo2003@gmail.com>
2024-06-24 19:03:06 +0000

    "Updated CSS class for div element in CartPage component"

--- src/app/(app)/cart/page.tsx
+++ src/app/(app)/cart/page.tsx
@@ -5,7 +5,7 @@ import CartPage from "./components/CartPage";
 function Page() {
   return (
     <div className="flex items-center justify-center">
-      <div className="md:w-[90%]">
+      <div className="w-full md:w-[90%]">
         <CartPage />
       </div>{" "}
     </div>
