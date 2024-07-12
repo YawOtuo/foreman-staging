352aa2ab9f8e06ef7fc7c6ef9621decb81d60e00
etornam <navidben45@gmail.com>
2024-07-12 03:16:05 +0000

    Fixed Issue with Auth logging

--- src/lib/hooks/useAuthState.ts
+++ src/lib/hooks/useAuthState.ts
@@ -13,7 +13,6 @@ export default function useAuthState(auth: any) {
     const unsubscribe = onAuthStateChanged(
       auth,
       async (userAuth: any) => {
-        console.log('userAuth', userAuth.providerData?.[0]?.email)
         try {
           if (userAuth) {
             // Set Firebase details
--- src/lib/types/product.ts
+++ src/lib/types/product.ts
@@ -1,18 +1,61 @@
-import { Category } from "./category";
-
-export interface Product {
+export interface Image {
   id: number;
-  name: string;
-  price: number;
-  description: string
-  category: Category
-  images: ProductImages[]
-  availability: string
+  image: string;
+  is_main: boolean;
 }
 
+export interface Variant {
+  id: number;
+  sku: string;
+  name: string;
+  images: Image[];
+  brief_description: string;
+  detailed_description: string;
+  size: string | null;
+  length: number | null;
+  width: number | null;
+  price: string;
+  availability: string;
+  created_at: string;
+  updated_at: string;
+  product: number;
+  related_products: RelatedProduct[];
+}
 
-interface ProductImages {
-  id: number
-  image: string 
-  is_main: boolean
+export interface RelatedProduct {
+  id: number;
+  name: string;
+  images: Image[];
+  category: {
+    id: number;
+    name: string;
+    image: string;
+    units_of_measurement: {
+      unit: string;
+      description: string;
+    }[];
+  };
+  description: string;
+  availability: string;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  image: string;
+  units_of_measurement: {
+    unit: string;
+    description: string;
+  }[];
+}
+
+export interface Product {
+  price: number;
+  id: number;
+  name: string;
+  description: string;
+  category: Category;
+  availability: string;
+  variants: Variant[];
+  images: Image[];
 }
