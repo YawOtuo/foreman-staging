b0fe7a4d32cdf3a94d51b1a2db34888ba37256d5
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-07-20 10:54:45 +0000

    new and improved maps code

--- src/app/(app)/checkout/map.tsx
+++ src/app/(app)/checkout/map.tsx
@@ -1,5 +1,14 @@
+"use client";
+import { Loader } from "@googlemaps/js-api-loader";
 import React, { useEffect, useRef, useState } from "react";
 import { useFormContext } from "react-hook-form";
+
+const ACCRA_BOUNDS = {
+  north: 5.7718,
+  south: 5.5038,
+  east: -0.1104,
+  west: -0.3098,
+};
 
 const Map: React.FC = () => {
   const mapRef = useRef<HTMLDivElement>(null);
@@ -7,106 +16,118 @@ const Map: React.FC = () => {
   const [mapInstance, setMapInstance] = useState<google.maps.Map | null>(null);
   const [marker, setMarker] =
     useState<google.maps.marker.AdvancedMarkerElement | null>(null);
-  const {
-    setValue,
-    register,
-    formState: { errors },
-  } = useFormContext();
-  const [coordinates, setCoordinates] = useState<string>("");
+  const { setValue, register } = useFormContext();
 
   useEffect(() => {
-    const loadGoogleMaps = () => {
-      const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=places,marker&v=beta`;
-      script.async = true;
-      script.defer = true;
-      script.onload = initMap;
-      script.onerror = () => console.error("Failed to load Google Maps API");
-      document.head.appendChild(script);
-
-      return () => {
-        document.head.removeChild(script);
-      };
-    };
-
-    const cleanup = loadGoogleMaps();
-    return cleanup;
-  }, []);
-
-  const initMap = () => {
-    if (!mapRef.current || !window.google) return;
-
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: { lat: 5.6037, lng: -0.187 },
-      zoom: 17,
-      mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID,
-      mapTypeControl: false,
-      streetViewControl: false,
-      fullscreenControl: false,
-    });
-
-    const newMarker = new window.google.maps.marker.AdvancedMarkerElement({
-      map,
-      position: map.getCenter(),
-    });
-
-    setMapInstance(map);
-    setMarker(newMarker);
-
-    if (searchInputRef.current) {
-      initSearchBox(map, newMarker);
-    }
-
-    map.addListener("click", (e: google.maps.MapMouseEvent) => {
-      if (e.latLng) {
-        newMarker.position = e.latLng;
-        updateCoordinatesAndAddress(e.latLng);
-      }
-    });
-  };
-
-  const initSearchBox = (
-    map: google.maps.Map,
-    marker: google.maps.marker.AdvancedMarkerElement
-  ) => {
-    if (!searchInputRef.current) return;
-
-    const searchBox = new window.google.maps.places.SearchBox(
-      searchInputRef.current
-    );
-    map.controls[window.google.maps.ControlPosition.TOP_CENTER].push(
-      searchInputRef.current
-    );
-
-    map.addListener("bounds_changed", () => {
-      searchBox.setBounds(map.getBounds() as google.maps.LatLngBounds);
-    });
-
-    searchBox.addListener("places_changed", () => {
-      const places = searchBox.getPlaces();
-      if (places?.length === 0) return;
-
-      const bounds = new window.google.maps.LatLngBounds();
-      places?.forEach((place) => {
-        if (!place.geometry || !place.geometry.location) return;
-
-        marker.position = place.geometry.location;
-        updateCoordinatesAndAddress(place.geometry.location);
-
-        if (place.geometry.viewport) {
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
+    const initMap = async () => {
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+        version: "weekly",
+        libraries: ["places", "marker"],
       });
 
-      map.fitBounds(bounds);
-    });
-  };
+      try {
+        const { Map } = await loader.importLibrary("maps");
+        const { AdvancedMarkerElement } = await loader.importLibrary("marker");
+        const { SearchBox } = await loader.importLibrary("places");
+
+        if (!mapRef.current) return;
+
+        const map = new Map(mapRef.current, {
+          center: { lat: 5.6037, lng: -0.187 },
+          zoom: 17,
+          mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID,
+          mapTypeControl: false,
+          streetViewControl: false,
+          fullscreenControl: false,
+        });
+
+        setMapInstance(map);
+
+        const newMarker = new AdvancedMarkerElement({
+          map,
+          position: map.getCenter(),
+        });
+
+        setMarker(newMarker);
+
+        if (searchInputRef.current) {
+          const searchBox = new SearchBox(searchInputRef.current);
+          map.controls[google.maps.ControlPosition.TOP_CENTER].push(
+            searchInputRef.current
+          );
+
+          // setting bounds for Accra
+          const accraBounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(ACCRA_BOUNDS.south, ACCRA_BOUNDS.west),
+            new google.maps.LatLng(ACCRA_BOUNDS.north, ACCRA_BOUNDS.east)
+          );
+
+          // setting the bounds and restricting to only Ghana
+          searchBox.setBounds(accraBounds);
+
+          const autocomplete = new google.maps.places.Autocomplete(
+            searchInputRef.current,
+            {
+              bounds: accraBounds,
+              componentRestrictions: { country: "gh" },
+              strictBounds: true,
+            }
+          );
+
+          autocomplete.addListener("place_changed", () => {
+            const place = autocomplete.getPlace();
+            if (!place.geometry || !place.geometry.location) return;
+
+            newMarker.position = place.geometry.location;
+            updateCoordinatesAndAddress(place.geometry.location);
+
+            if (place.geometry.viewport) {
+              map.fitBounds(place.geometry.viewport);
+            } else {
+              map.setCenter(place.geometry.location);
+              map.setZoom(17);
+            }
+          });
+
+          searchBox.addListener("places_changed", () => {
+            const places = searchBox.getPlaces();
+            if (places?.length === 0) return;
+
+            const bounds = new google.maps.LatLngBounds();
+            places?.forEach((place) => {
+              if (!place.geometry || !place.geometry.location) return;
+
+              newMarker.position = place.geometry.location;
+              updateCoordinatesAndAddress(place.geometry.location);
+
+              if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+              } else {
+                bounds.extend(place.geometry.location);
+              }
+            });
+
+            map.fitBounds(bounds);
+          });
+        }
+
+        map.addListener("click", (e: google.maps.MapMouseEvent) => {
+          if (e.latLng) {
+            newMarker.position = e.latLng;
+            updateCoordinatesAndAddress(e.latLng);
+          }
+        });
+      } catch (error) {
+        console.error("Error initializing Google Maps:", error);
+      }
+    };
+
+    initMap();
+  }, []);
 
   const updateCoordinatesAndAddress = (location: google.maps.LatLng) => {
-    setCoordinates(`${location.lat()}, ${location.lng()}`);
-    const geocoder = new window.google.maps.Geocoder();
+    const geocoder = new google.maps.Geocoder();
     geocoder.geocode({ location }, (results, status) => {
       if (status === "OK" && results && results[0]) {
         setValue("address.location", results[0].formatted_address, {
