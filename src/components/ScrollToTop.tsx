ba9e852260a65140f4f51b39e6b9e0752e23e03a
YawOtuo <yotuo2003@gmail.com>
2024-08-09 23:12:46 +0000

    "Update Navbar z-index and rename ScrollToTop function"

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,12 +1,12 @@
 "use client"
 import { useEffect } from 'react';
 import { usePathname } from 'next/navigation';
-
-export default function ScrollToTop() {
+export default function useScrollToTop() {
   const pathname = usePathname();
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    console.log('going to top')
   }, [pathname]);
 
   return null;
