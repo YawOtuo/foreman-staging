11710a52395a7eedb2c4987f73fdc576b60c972f
YawOtuo <yotuo2003@gmail.com>
2024-09-27 16:43:04 +0000

    "Updated ProductLoadingSkeleton component with new layout and additional skeleton elements"

--- src/app/(app)/product/[product_id]/ProductLoadingSkeleton.tsx
+++ src/app/(app)/product/[product_id]/ProductLoadingSkeleton.tsx
@@ -2,17 +2,25 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 function ProductLoadingSkeleton() {
   return (
-    <div className="flex flex-col lg:flex-row items-start lg:items-start justify-center gap-10">
-      <Skeleton className="w-full h-[400px] " />
-      <div className="flex flex-col gap-5 w-full">
-        <Skeleton className=" w-[50%] h-[40px]  " />
+    <div className="flex flex-col gap-20">
+      <div className="flex flex-col lg:flex-row items-start lg:items-start justify-center gap-10">
+        <Skeleton className="w-full h-[500px] " />
 
-        <Skeleton className="w-[95%]  h-[120px]" />
-        <div className="flex flex-col gap-2">
-          <Skeleton className="w-[70%]  h-[40px]" />
-          <Skeleton className="w-[70%]  h-[40px]" />
-          <Skeleton className="w-[70%]  h-[40px]" />
+        <div className="flex flex-col gap-5 w-full">
+          <Skeleton className=" w-[50%] h-[40px]  " />
+
+          <Skeleton className="w-[95%]  h-[120px]" />
+          <div className="flex flex-col gap-2">
+            <Skeleton className="w-[70%]  h-[40px]" />
+            <Skeleton className="w-[70%]  h-[40px]" />
+            <Skeleton className="w-[70%]  h-[40px]" />
+          </div>
         </div>
+      </div>
+      <div className="flex flex-col lg:flex-row items-center gap-5">
+        {Array.from({ length: 4 }).map((r: any) => (
+          <Skeleton key={r} className="w-full lg:w-[400px] h-[200px] lg:h-[400px]" />
+        ))}
       </div>
     </div>
   );
